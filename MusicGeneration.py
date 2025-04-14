227fc01511b8e7c2578e441cff1d656edc5302a2
SuryaSunanda <sunanda.22bce20515@vitapstudent.ac.in>
2025-04-15 01:00:14 +0530

    preprocessed song dataset

--- MusicGeneration.py
+++ MusicGeneration.py
@@ -7,6 +7,11 @@ env['musescoreDirectPNGPath'] = '/Applications/MuseScore 4.app/Contents/MacOS/ms
 env['musicxmlPath'] = '/Applications/MuseScore 4.app/Contents/MacOS/mscore'
 
 KERN_DATASET_PATH = "/Users/sunny/Downloads/essen/europa/deutschl/test"
+SAVE_DIR = "dataset"
+SINGLE_FILE_DATASET = "file_dataset"
+SEQUENCE_LENGTH = 64
+
+#durations are expressed in quarter length
 ACCEPTABLE_DURATIONS = [
     0.25,
     0.5,
@@ -63,33 +68,104 @@ def transpose(song):
   transposed_song = song.transpose(interval)
   return transposed_song
 
+# this function gets the song as an input as the music21 object and gives back the string
+def encode_song(song, time_step=0.25):
+    """Converts a score into a time-series-like music representation. Each item in the encoded list represents 'min_duration'
+    quarter lengths. The symbols used at each step are: integers for MIDI notes, 'r' for representing a rest, and '_'
+    for representing notes/rests that are carried over into a new time step. Here's a sample encoding:
+
+        ["r", "_", "60", "_", "_", "_", "72" "_"]
+
+    :param song (m21 stream): Piece to encode
+    :param time_step (float): Duration of each time step in quarter length
+    :return:
+    """
+
+    encoded_song = []
+
+    for event in song.flat.notesAndRests:
+
+      # handle notes
+      if isinstance(event, m21.note.Note):
+        symbol = event.pitch.midi  # 60
+      # handle rests
+      elif isinstance(event, m21.note.Rest):
+        symbol = "r"
+
+      # convert the note/rest into time series notation
+      steps = int(event.duration.quarterLength / time_step)
+      for step in range(steps):
+
+        # if it's the first time we see a note/rest, let's encode it. Otherwise, it means we're carrying the same
+        # symbol in a new time step
+        if step == 0:
+          encoded_song.append(symbol)
+        else:
+          encoded_song.append("_")
+
+    # cast encoded song to str
+    encoded_song = " ".join(map(str, encoded_song))
+
+    return encoded_song
+
 
 def preprocess(dataset_path):
-  pass
 
   # load the folk songs
   print("Loading Songs...")
   songs = load_songs_in_kern(dataset_path)
   print(f"Loaded{len(songs)} songs.")
 
-  for song in songs:
+  for i, song in enumerate(songs):
 
     # filter out songs that have non-acceptable durations
     if not has_acceptable_durations(song, ACCEPTABLE_DURATIONS):
       continue
 
-  # transpose songs to Cmaj/min
-  song = transpose(song)
+    # transpose songs to Cmaj/min
+    song = transpose(song)
 
-  # encode songs with music time series representation
+    # encode songs with music time series representation
+    encoded_song = encode_song(song)
 
-  # save songs to text file
+    # save songs to text file
+    save_path = os.path.join(SAVE_DIR, str(i))
+    with open(save_path, "w") as fp:
+      fp.write(encoded_song)
+
+def load(file_path):
+  with open(file_path, "r") as fp:
+    song = fp.read()
+  return song
+
+def create_single_file_dataset(dataset_path, file_dataset_path, sequence_length):
+  new_song_delimiter = "/ " * sequence_length
+  songs = ""
+
+  # load encoded songs and add delimiters
+  for path, _, files in os.walk(dataset_path):
+    for file in files:
+      file_path = os.path.join(path, file)
+      song = load(file_path)
+      songs = songs + song + " " + new_song_delimiter
+
+  songs = songs[:-1]
+
+  # save string that contains all dataset
+  with open(file_dataset_path, "w") as fp:
+    fp.write(songs)
+
+  return songs
 
 
 if __name__ == "__main__":
   songs = load_songs_in_kern(KERN_DATASET_PATH)
   print(f"Loaded {len(songs)} songs.")
   song = songs[0]
+
+  preprocess(KERN_DATASET_PATH)
+
+  songs = create_single_file_dataset(SAVE_DIR, SINGLE_FILE_DATASET, SEQUENCE_LENGTH)
 
   print(f"Has acceptable duration? {has_acceptable_durations(song, ACCEPTABLE_DURATIONS)}")
 
@@ -99,9 +175,3 @@ if __name__ == "__main__":
   input("Press Enter after closing MuseScore to continue...")
   transposed_song.show()
 
-
-
-
-
-
-# hi
