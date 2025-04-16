7f6c7bf40df523e21a3a81eba8e8d04d20724863
SuryaSunanda <sunanda.22bce20515@vitapstudent.ac.in>
2025-04-17 01:33:14 +0530

    trained data

--- MusicGeneration.py
+++ MusicGeneration.py
@@ -1,14 +1,18 @@
 #Preprocessing Dataset for Melody Generation
 import os
+import json
+import tensorflow.keras as keras
 import music21 as m21
+import numpy as np
 
 env = m21.environment.Environment()
 env['musescoreDirectPNGPath'] = '/Applications/MuseScore 4.app/Contents/MacOS/mscore'
 env['musicxmlPath'] = '/Applications/MuseScore 4.app/Contents/MacOS/mscore'
 
-KERN_DATASET_PATH = "/Users/sunny/Downloads/essen/europa/deutschl/test"
+KERN_DATASET_PATH = "/Users/sunny/Downloads/essen/europa/deutschl/erk"
 SAVE_DIR = "dataset"
 SINGLE_FILE_DATASET = "file_dataset"
+MAPPING_PATH = "mapping.json"
 SEQUENCE_LENGTH = 64
 
 #durations are expressed in quarter length
@@ -24,7 +28,7 @@ ACCEPTABLE_DURATIONS = [
 ]
 
 def load_songs_in_kern(dataset_path):
-  # go throught all the files in dataset and load them with music21
+  # go through all the files in dataset and load them with music21
   # os walk recursively goes through all the files given a parent folder
   # path is referenced to the path of the current folder
   # subdirs - all the sub folders in the path, files are all the files in the path
@@ -157,21 +161,76 @@ def create_single_file_dataset(dataset_path, file_dataset_path, sequence_length)
 
   return songs
 
+def create_mapping(songs, mapping_path):
+  mappings =  {}
+
+  # identify the vocabulary
+  songs = songs.split()
+  vocabulary = list(set(songs))
+
+  # create mappings
+  for i, symbol in enumerate(vocabulary):
+    mappings[symbol] = i
+
+  # save vocabulary to a json file
+  with open(mapping_path, "w") as fp:
+    json.dump(mappings, fp, indent=4)
+
+
+def convert_songs_to_int(songs):
+
+  int_songs = []
+
+  # load mappings
+  with open(MAPPING_PATH, "r") as fp:
+    mappings = json.load(fp)
+
+  # cast songs string to a list
+  songs = songs.split()
+
+  # map songs to int
+  for symbol in songs:
+    int_songs.append(mappings[symbol])
+
+  return int_songs
+
+def generate_training_sequences(sequence_length):
+    """Create input and output data samples for training. Each sample is a sequence.
+
+    :param sequence_length (int): Length of each sequence. With a quantisation at 16th notes, 64 notes equates to 4 bars
+
+    :return inputs (ndarray): Training inputs
+    :return targets (ndarray): Training targets
+    """
+
+    # load songs and map them to int
+    songs = load(SINGLE_FILE_DATASET)
+    int_songs = convert_songs_to_int(songs)
+
+    inputs = []
+    targets = []
+
+    # generate the training sequences
+    num_sequences = len(int_songs) - sequence_length
+    for i in range(num_sequences):
+      inputs.append(int_songs[i:i + sequence_length])
+      targets.append(int_songs[i + sequence_length])
+
+    # one-hot encode the sequences
+    vocabulary_size = len(set(int_songs))
+    # inputs size: (# of sequences, sequence length, vocabulary size)
+    inputs = keras.utils.to_categorical(inputs, num_classes=vocabulary_size)
+    targets = np.array(targets)
+
+    return inputs, targets
+
+
+def main():
+  preprocess(KERN_DATASET_PATH)
+  songs = create_single_file_dataset(SAVE_DIR, SINGLE_FILE_DATASET, SEQUENCE_LENGTH)
+  create_mapping(songs, MAPPING_PATH)
+  inputs, targets = generate_training_sequences(SEQUENCE_LENGTH)
+
 
 if __name__ == "__main__":
-  songs = load_songs_in_kern(KERN_DATASET_PATH)
-  print(f"Loaded {len(songs)} songs.")
-  song = songs[0]
-
-  preprocess(KERN_DATASET_PATH)
-
-  songs = create_single_file_dataset(SAVE_DIR, SINGLE_FILE_DATASET, SEQUENCE_LENGTH)
-
-  print(f"Has acceptable duration? {has_acceptable_durations(song, ACCEPTABLE_DURATIONS)}")
-
-  transposed_song = transpose(song)
-
-  song.show()
-  input("Press Enter after closing MuseScore to continue...")
-  transposed_song.show()
-
+  main()
